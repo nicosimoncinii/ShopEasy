db0b4e7af9949d9150a06b3741afdf70405b9ed7
NiccoAndreozzi <niccoloandreozzi1@gmail.com>
2026-06-18 12:00:48 +0200

    Correzione backend

---
+++ backend/shopeasy/src/test/java/it/shopeasy/shopeasy/ShopeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package it.shopeasy.shopeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

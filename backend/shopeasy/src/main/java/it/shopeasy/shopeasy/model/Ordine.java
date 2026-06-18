944a0586e4582dda39627fb5d42a9f4ed4c4a461
nicosimoncini <simoncininicos@gmail.com>
2026-06-18 16:11:32 +0200

    MODEL CLASSI

--- backend/shopeasy/src/main/java/it/shopeasy/shopeasy/model/Ordine.java
+++ backend/shopeasy/src/main/java/it/shopeasy/shopeasy/model/Ordine.java
@@ -1,5 +1,78 @@
 package it.shopeasy.shopeasy.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ordini")
 public class Ordine {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime data;
+
+    private Double totale;
+
+    @Enumerated(EnumType.STRING)
+    private StatoOrdine stato;
+
+    @ManyToOne
+    @JoinColumn(name = "utente_id")
+    private Utente utente;
+
+
+    public Ordine() {
+    }
+
+    public Ordine(LocalDateTime data,
+                  Double totale,
+                  StatoOrdine stato,
+                  Utente utente) {
+
+        this.data = data;
+        this.totale = totale;
+        this.stato = stato;
+        this.utente = utente;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public Double getTotale() {
+        return totale;
+    }
+
+    public void setTotale(Double totale) {
+        this.totale = totale;
+    }
+
+    public StatoOrdine getStato() {
+        return stato;
+    }
+
+    public void setStato(StatoOrdine stato) {
+        this.stato = stato;
+    }
+
+    public Utente getUtente() {
+        return utente;
+    }
+
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+    }
+
 }
--- backend/shopeasy/src/main/java/it/shopeasy/shopeasy/model/Prodotto.java
+++ backend/shopeasy/src/main/java/it/shopeasy/shopeasy/model/Prodotto.java
@@ -1,5 +1,97 @@
 package it.shopeasy.shopeasy.model;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "prodotti")
 public class Prodotto {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    private String descrizione;
+
+    @Column(nullable = false)
+    private Double prezzo;
+
+    @Column(nullable = false)
+    private Integer quantita;
+
+    private String immagine;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+
+    public Prodotto() {
+    }
+
+    public Prodotto(String nome, String descrizione, Double prezzo,
+                    Integer quantita, String immagine, Categoria categoria) {
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.quantita = quantita;
+        this.immagine = immagine;
+        this.categoria = categoria;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public Double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(Double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public Integer getQuantita() {
+        return quantita;
+    }
+
+    public void setQuantita(Integer quantita) {
+        this.quantita = quantita;
+    }
+
+    public String getImmagine() {
+        return immagine;
+    }
+
+    public void setImmagine(String immagine) {
+        this.immagine = immagine;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 }

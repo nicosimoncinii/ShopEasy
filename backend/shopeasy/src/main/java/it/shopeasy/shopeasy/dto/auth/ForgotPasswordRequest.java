bd34845a6621804e1fff3bd003188b47f73413fc
nicosimoncini <simoncininicos@gmail.com>
2026-06-18 17:36:03 +0200

    Aggiunta auth

--- backend/shopeasy/src/main/java/it/shopeasy/shopeasy/dto/auth/ForgotPasswordRequest.java
+++ backend/shopeasy/src/main/java/it/shopeasy/shopeasy/dto/auth/ForgotPasswordRequest.java
@@ -1,5 +1,26 @@
 package it.shopeasy.shopeasy.dto.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class ForgotPasswordRequest {
-    
+
+    @NotBlank(message = "L'email è obbligatoria")
+    @Email(message = "Formato email non valido")
+    private String email;
+
+    public ForgotPasswordRequest() {
+    }
+
+    public ForgotPasswordRequest(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }

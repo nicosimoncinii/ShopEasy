445f6739af7549328034824aa3cd64e4e3250f4f
NiccoAndreozzi <niccoloandreozzi1@gmail.com>
2026-06-22 17:23:50 +0200

    Aggiunti permessi per API

--- backend/shopeasy/src/main/java/it/shopeasy/security/SecurityConfig.java
+++ backend/shopeasy/src/main/java/it/shopeasy/security/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.UsmvernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -49,9 +49,8 @@ public class SecurityConfig {
             .sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**").permitAll()
-                .requestMatchers("/api/products").permitAll()
-                .requestMatchers("/api/products/**").permitAll()
+                .requestMatchers("/api/**").permitAll()
+
                 .anyRequest().authenticated()
             )
             .authenticationProvider(authenticationProvider())

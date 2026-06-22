ed8cc9ad102864a16d6c3023969fe6fc87787dd4
nicosimoncini <simoncininicos@gmail.com>
2026-06-22 11:43:11 +0200

    Security

--- backend/shopeasy/src/main/java/it/shopeasy/repository/UtenteRepository.java
+++ backend/shopeasy/src/main/java/it/shopeasy/repository/UtenteRepository.java
@@ -2,7 +2,9 @@ package it.shopeasy.repository;
 
 import it.shopeasy.model.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface UtenteRepository extends JpaRepository<Utente, Long>{
-
+public interface UtenteRepository extends JpaRepository<Utente, Long> {
+    Optional<Utente> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
--- backend/shopeasy/src/main/java/it/shopeasy/security/JwtService.java
+++ backend/shopeasy/src/main/java/it/shopeasy/security/JwtService.java
@@ -1,24 +1,49 @@
 package it.shopeasy.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import it.shopeasy.model.Utente;
 import org.springframework.stereotype.Service;
-
 import javax.crypto.SecretKey;
-
+import java.util.Date;
 
 @Service
 public class JwtService {
+    private final SecretKey secretKey = Keys.hmacShaKeyFor(
+        "chiave-32".getBytes()
+    );
+    private final long durata = 3000 * 60 * 60;
 
-    private final SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-    private final long durata = 1000 * 60 * 60;
-
+    private Claims estraiClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
 
     public String generaToken(Utente utente) {
         return Jwts.builder()
-                .setSubject(utente.getEmail());
+                .subject(utente.getEmail())
+                .claim("ruolo", utente.getRuolo().getNome())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + durata))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String estraiEmail(String token) {
+        return estraiClaims(token).getSubject();
+    }
+
+    private boolean isTokenScaduto(String token) {
+        return estraiClaims(token).getExpiration().before(new Date());
+    }
+
+    public boolean isTokenValido(String token, String email) {
+        return estraiEmail(token).equals(email) && !isTokenScaduto(token);
     }
 
 }
+

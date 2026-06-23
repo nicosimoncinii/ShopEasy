5d327595eb03873c99ade65c2c3f303c81d29885
nicosimoncini <simoncininicos@gmail.com>
2026-06-23 14:55:50 +0200

    swagger

---
+++ backend/shopeasy/src/main/java/it/shopeasy/dto/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package it.shopeasy.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("ShopEasy API")
+                        .version("1.0")
+                        .description("API per il e-commerce ShopEasy")
+                        .contact(new Contact()
+                                .name("ShopEasy Team")
+                                .email("support@shopeasy.it")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")));
+    }
+}

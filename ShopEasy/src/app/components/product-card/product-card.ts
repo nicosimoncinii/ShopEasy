d9cb4d996c02f76bfec02011ad9d69bf1a088277
ambra-bae <ambra-bae>
2026-06-29 10:06:08 +0200

    aggiornamento wishlist e product card

--- ShopEasy/src/app/components/product-card/product-card.ts
+++ ShopEasy/src/app/components/product-card/product-card.ts
@@ -1,14 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgIconComponent } from '@ng-icons/core';
 import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgIconComponent],
   templateUrl: './product-card.html',
   styleUrl: './product-card.scss',
 })
 export class ProductCard {
   @Input() product!: Product;
+  @Output() aggiungiAlCarrello = new EventEmitter<Product>();
+  @Output() toggleWishlist = new EventEmitter<Product>();
+  @Input() isInWishlist: boolean = false;
+
+  onAggiungiAlCarrello() {
+    this.aggiungiAlCarrello.emit(this.product);
+  }
+
+  onToggleWishlist() {
+    this.toggleWishlist.emit(this.product);
+  }
 }
--- ShopEasy/src/app/components/product-list/product-list.ts
+++ ShopEasy/src/app/components/product-list/product-list.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductCard } from '../product-card/product-card';
 import { CartService } from '../../services/cart.service';
+import { WishlistService } from '../../services/wishlist.service';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product-list',
@@ -11,9 +13,25 @@ import { CartService } from '../../services/cart.service';
   styleUrl: './product-list.scss',
 })
 export class ProductList {
-  constructor(public cartService: CartService) {}
+
+  constructor(
+      public cartService: CartService,
+      public wishlistService: WishlistService
+  ) {}
 
   get products() {
     return this.cartService.prodottiTutti;
+  }
+
+  onAggiungiAlCarrello(product: Product) {
+    this.cartService.aggiungiProdotto(product);
+  }
+
+  onToggleWishlist(product: Product) {
+    if (this.wishlistService.isInWishlist(product.id)) {
+      this.wishlistService.rimuovi(product.id);
+    } else {
+      this.wishlistService.aggiungi(product);
+    }
   }
 }

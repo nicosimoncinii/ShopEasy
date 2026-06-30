4cbac2d48599dbaff053710acef61a26145c89c1
loreroggi08 <lorenzoroggi08@gmail.com>
2026-06-30 15:09:58 +0200

    aggiunta area personale (completa)

--- ShopEasy/src/app/app.routes.ts
+++ ShopEasy/src/app/app.routes.ts
@@ -7,6 +7,8 @@ import { RegisterComponent } from './pages/register/register'; // Corretto il no
 import { Cart } from './pages/cart/cart';
 import { Wishlist } from './pages/wishlist/wishlist';
 import { NotFoundComponent } from './pages/not-found/not-found';
+import { PersonalArea } from './pages/personal-area/personal-area';
+
 
 export const routes: Routes = [
     { path: '', component: Home },
@@ -15,5 +17,6 @@ export const routes: Routes = [
     { path: 'register', component: RegisterComponent }, // Corretto il nome del componente
     { path: 'cart', component: Cart },
     { path: 'wishlist', component: Wishlist },
+    { path: 'personal-area', component: PersonalArea },
     { path: '**', component: NotFoundComponent }
 ];

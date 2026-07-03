7c00cd504b0abde97aff5ec7b65017329b240eac
loreroggi08 <lorenzoroggi08@gmail.com>
2026-07-03 10:03:44 +0200

    risolto backend di recupero psw

--- backend/shopeasy/src/main/java/it/shopeasy/repository/PasswordResetTokenRepository.java
+++ backend/shopeasy/src/main/java/it/shopeasy/repository/PasswordResetTokenRepository.java
@@ -3,10 +3,13 @@ package it.shopeasy.repository;
 import it.shopeasy.model.PasswordResetToken;
 import it.shopeasy.model.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+
 
 import java.util.Optional;
 
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
     Optional<PasswordResetToken> findByToken(String token);
+    @Modifying // <--- Obbligatorio per le query di cancellazione derivative
     void deleteByUtente(Utente utente);
 }
